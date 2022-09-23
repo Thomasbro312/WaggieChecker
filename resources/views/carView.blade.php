796aee75c653499f0b2059ce115cfb27c63faeaa
Thomasbro312 <73693551+Thomasbro312@users.noreply.github.com>
2022-09-23 10:25:17 +0200

    Added information to carView.blade.php

--- resources/views/carView.blade.php
+++ resources/views/carView.blade.php
@@ -10,13 +10,26 @@
 
     <!-- Styles -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <style>
+        .container{
+            display: flex;
+            flex-direction: row;
+            flex-wrap: nowrap;
+            justify-content: space-around;
+            align-items: center;
+            align-content: stretch;
+        }
+        .carDescription{
+            width: 400px;
+        }
+    </style>
 <?php
     $sc = new \allejo\Socrata\SodaClient('opendata.rdw.nl');
     $ds = new SodaDataSet($sc, 'm9d7-ebf2');
     $soql = new \allejo\Socrata\SoqlQuery();
-    $soql->select("merk","kenteken","handelsbenaming")->where("kenteken = '99TJNP'");
+    $soql->where("kenteken = '99TJNP'");
 
-    $car1 = $ds->getDataset($soql);
+    $car1 = $ds->getDataset($soql)
 ?>
     <div class="container-md">
     <nav class="navbar navbar-light bg-light">
@@ -31,10 +44,27 @@
 </div>
 
     <body>
-        <div class="carTitle">
-            <h1>{{$car1[0]["merk"]}}</h1>
+        <div class="container">
+            <div class="carTitle">
+                <h1>{{$car1[0]["handelsbenaming"] }}</h1>
+            </div>
         </div>
-        <div class="carDescription">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti eius fugiat, iure minus nobis quas quos veritatis! Accusantium delectus distinctio ipsa laboriosam minus natus quia quidem similique, soluta tempore?</p>
+        <div class="container">
+            <div class="carDescription">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti eius fugiat, iure minus nobis quas quos veritatis! Accusantium delectus distinctio ipsa laboriosam minus natus quia quidem similique, soluta tempore?</p>
+            </div>
+
+            <div class="img_container">
+                <img src="{{asset("storage/car1.jpg")}}" alt="">
+            </div>
+        </div>
+        <div class="containerSpecs">
+            <p>Merk: {{$car1[0]['merk']}}</p>
+            <p>Model: {{$car1[0]['handelsbenaming']}}</p>
+            <p>Kleur: {{$car1[0]['eerste_kleur']}}</p>
+            <p>Inrichting: {{$car1[0]['inrichting']}}</p>
+            <p>APK vervaldatum: {{$car1[0]['vervaldatum_apk_dt']}}</p>
+            <p>Aantal cilinders: {{$car1[0]['aantal_cilinders']}}</p>
+            <p>Kenteken: {{$car1[0]['kenteken']}}</p>
         </div>
     </body>
