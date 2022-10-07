9042ce3f4ec9582c017d4f29606e52855c4a1dcc
Joey Blommers <joeyblommers2004@gmail.com>
2022-10-07 16:26:09 +0200

    Think its nice

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.auth.base')
 @section('content')
     <section id="contact" class="contact">
-        <div class="container" data-aos="fade-up">
+        <div class="container">
 
             <div class="section-header">
-                <h2>Contacteer ons.</h2>
-                <p>Vul dit formulier in om fouten in onze website te melden en of vragen te stellen over het gebruik van de website</p>
+                <h2>Jouw account</h2>
+                <p>Log in jouw account of maak een account aan.</p>
             </div>
 
             <div class="row gx-lg-0 gy-4">
@@ -17,7 +17,7 @@
                             <i class="bi bi-envelope flex-shrink-0"></i>
                             <div>
                                 <h4>Voordelen</h4>
-                                <p>Auto verkopen, berichten sturen naar mensen.</p>
+                                <p>Je auto verkopen<br> berichten sturen naar mensen.</p>
                             </div>
                         </div>
 
@@ -34,21 +34,17 @@
 
                 <div class="col-lg-8">
                     <form action="forms/contact.php" method="post" role="form" class="php-email-form">
-                        <div class="row">
-                            <div class="col-md-6 form-group">
-                                <input type="text" name="name" class="form-control" id="name" placeholder="Jouw naam" required>
-                            </div>
-                            <div class="col-md-6 form-group mt-3 mt-md-0">
-                                <input type="email" class="form-control" name="email" id="email" placeholder="Jouw email" required>
+                        <div class="row justify-content-center align-self-center">
+                            <div class="col-md-6 form-group mt-3 mt-md-0 ">
+                                <input type="email" class="form-control" name="email" id="email" placeholder="wie@waggiechecker.nl" required>
                             </div>
                         </div>
-                        <div class="form-group mt-3">
-                            <input type="text" class="form-control" name="subject" id="subject" placeholder="Onderwerp" required>
+                        <div class="row justify-content-center align-self-center">
+                            <div class="form-group col-md-6 mt-3">
+                                <input type="text" class="form-control" name="password" id="password" placeholder="Je wachtwoord" required>
+                            </div>
                         </div>
-                        <div class="form-group mt-3">
-                            <textarea class="form-control" name="message" rows="7" placeholder="Stel hier jouw vraag!" required></textarea>
-                        </div>
-                        <div class="text-center"><button type="submit">Login</button></div>
+                        <div class="text-center mt-4"><button type="submit">Login</button></div>
                     </form>
                 </div><!-- End Contact Form -->
 
