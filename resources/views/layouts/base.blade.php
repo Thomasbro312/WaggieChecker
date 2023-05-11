1081268d98c96cdb55309211de8b1e9f067ddc38
Thomasbro312 <73693551+Thomasbro312@users.noreply.github.com>
2023-05-11 12:56:14 +0200

    Added navbar

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css"  rel="stylesheet" />
     <title></title>
     @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
@@ -12,5 +14,6 @@
 <div class="w-full h-full">
     @yield('content')
 </div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
 </body>
 </html>
--- resources/views/non-auth/homepage.blade.php
+++ resources/views/non-auth/homepage.blade.php
@@ -1,6 +1,59 @@
 @extends('layouts.base')
 @section('content')
-    <h2>hallo</h2>
+
+    <nav class="border-gray-200 bg-[#388174]">
+        <div class="flex flex-wrap justify-between mx-auto p-4">
+            <a href="#" class="flex">
+                <span class="text-left text-white text-2xl font-semibold whitespace-nowrap">WaggieChecker<span class="text-[#f96f59]">.</span> / CarBuddy<span class="text-[#f96f59]">.</span></span>
+            </a>
+            <button data-collapse-toggle="navbar-solid-bg" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden" aria-controls="navbar-solid-bg" aria-expanded="false">
+                <span class="sr-only">Open main menu</span>
+                <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+            </button>
+            <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
+                <ul class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent ">
+                    <li>
+                        <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-white md:p-0 md:hover:text-white hover:underline hover:underline-offset-8" aria-current="page">Hoofdpagina.</a>
+                    </li>
+                    <li>
+                        <a href="#" class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-white md:p-0  hover:underline hover:underline-offset-8">Aanbod.</a>
+                    </li>
+                    <li>
+                        <a href="#" class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-white md:p-0  hover:underline hover:underline-offset-8">Nieuws.</a>
+                    </li
+                    <li>
+                        <a href="#" class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-white md:p-0  hover:underline hover:underline-offset-8">Profile.</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     <form method="post" action="{{route('kenteken.plateCheck')}}">
         @csrf
         </div>
