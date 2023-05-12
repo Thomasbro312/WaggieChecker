49af9d98c675e3883471ca335119143639d0a971
Joey Blommers <joeyblommers2004@gmail.com>
2023-05-13 01:20:00 +0200

    Cache done

--- resources/views/non-auth/homepage.blade.php
+++ resources/views/non-auth/homepage.blade.php
@@ -31,19 +31,19 @@
         </div>
     </section>
 
-{{--    <form method="post" action="{{route('kenteken.plateCheck')}}">--}}
-{{--        @csrf--}}
-{{--        </div>--}}
-{{--        <div class="">--}}
-{{--            <div class="">--}}
-{{--                <div class=""></div>--}}
-{{--                <input type="text" placeholder="" name="kenteken" />--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--        <div class="form-group">--}}
-{{--            <input type="submit" value="Checken" class="btn-get-started">--}}
-{{--        </div>--}}
-{{--        --}}{{--                            </div>--}}
-{{--    </form>--}}
-{{--    <img src="" alt="">--}}
+    <form method="post" action="{{route('kenteken.plateCheck')}}">
+        @csrf
+        </div>
+        <div class="">
+            <div class="">
+                <div class=""></div>
+                <input type="text" placeholder="" name="kenteken" />
+            </div>
+        </div>
+        <div class="form-group">
+            <input type="submit" value="Checken" class="btn-get-started">
+        </div>
+                                    </div>
+    </form>
+    <img src="" alt="">
 @endsection
--- resources/views/non-auth/plate-result.blade.php
+++ resources/views/non-auth/plate-result.blade.php
@@ -6,55 +6,55 @@
                     <tbody>
                     <tr>
                         <td colspan="2">Merk:</td>
-                        <th>{{$car->merk}}</th>
+                        <th>{{$data['car']->merk}}</th>
                     </tr>
                     <tr>
                         <td colspan="2">Model:</td>
-                        <th>{{$car->handelsbenaming}}</th>
+                        <th>{{$data['car']->handelsbenaming}}</th>
                     </tr>
                     <tr>
                         <td colspan="2">Inrichting:</td>
-                        <th>{{$car->inrichting}}</th>
+                        <th>{{$data['car']->inrichting}}</th>
                     </tr>
-                    @if(!empty($power->nettomaximumvermogen))
+                    @if(!empty($data['power']->nettomaximumvermogen))
                         <tr>
                             <td colspan="2">Vermogen in KW:</td>
-                            <th>{{$power->nettomaximumvermogen}}</th>
+                            <th>{{$data['power']->nettomaximumvermogen}}</th>
                         </tr>
                     @endif
                     <tr>
                         <td colspan="2">Kleur:</td>
-                        <th>{{$car->eerste_kleur}}</th>
+                        <th>{{$data['car']->eerste_kleur}}</th>
                     </tr>
                     <tr>
                         <td colspan="2">APK vervaldatum:</td>
-                        <th><?php echo date("d-m-Y", strtotime($car->vervaldatum_apk_dt)) ?></th>
+                        <th><?php echo date("d-m-Y", strtotime($data['car']->vervaldatum_apk_dt)) ?></th>
                     </tr>
                     <tr>
                         <td colspan="2">Kenteken:</td>
-                        <th>{{$car->kenteken}}</th>
+                        <th>{{$data['car']->kenteken}}</th>
                     </tr>
                     <tr>
                         <td colspan="2">Massa Rijklaar:</td>
-                        <th>{{$car->massa_rijklaar}} KG</th>
+                        <th>{{$data['car']->massa_rijklaar}} KG</th>
                     </tr>
                     </tbody>
                 </table>
-                @if(!empty($power->brandstofverbruik_gecombineerd))
+                @if(!empty($data['power']->brandstofverbruik_gecombineerd))
                     <div class="section-heading"><h4>Brandstof Gebruik.</h4></div>
                     <table class="table table-striped table-hover">
                         <tbody>
                         <tr>
                             <td colspan="2">Gecombineerd:</td>
-                            <th>{{$power->brandstofverbruik_gecombineerd}}{{" l/100KM"}}</th>
+                            <th>{{$data['power']->brandstofverbruik_gecombineerd}}{{" l/100KM"}}</th>
                         </tr>
                         <tr>
                             <td colspan="2">Buiten stad:</td>
-                            <th>{{$power->brandstofverbruik_buiten}}{{" l/100KM"}}</th>
+                            <th>{{$data['power']->brandstofverbruik_buiten}}{{" l/100KM"}}</th>
                         </tr>
                         <tr>
                             <td colspan="2">Stad:</td>
-                            <th>{{$power->brandstofverbruik_stad}}{{" l/100KM"}}</th>
+                            <th>{{$data['power']->brandstofverbruik_stad}}{{" l/100KM"}}</th>
                         </tr>
                         </tbody>
                     </table>
@@ -64,89 +64,89 @@
                     <tbody>
                     <tr>
                         <td colspan="2">Inrichting:</td>
-                        <th>{{$car->inrichting}}</th>
+                        <th>{{$data['car']->inrichting}}</th>
                     </tr>
                     <tr>
                         <td colspan="2">Kleur:</td>
-                        <th>{{$car->eerste_kleur}}</th>
+                        <th>{{$data['car']->eerste_kleur}}</th>
                     </tr>
                     <tr>
                         <td colspan="2">Uitvoering:</td>
-                        <th>{{$car->uitvoering}}</th>
+                        <th>{{$data['car']->uitvoering}}</th>
                     </tr>
                     <tr>
                         <td colspan="2">Variant:</td>
-                        <th>{{$car->variant}}</th>
+                        <th>{{$data['car']->variant}}</th>
                     </tr>
                     <tr>
                         <td colspan="2">Wielbasis:</td>
-                        <th>{{$car->wielbasis}} MM</th>
+                        <th>{{$data['car']->wielbasis}} MM</th>
                     </tr>
                     <tr>
                         <td colspan="2">Eerste Toelating Internationaal:</td>
-                        <th><?php echo date("d-m-Y", strtotime($car->datum_eerste_toelating_dt)) ?></th>
+                        <th><?php echo date("d-m-Y", strtotime($data['car']->datum_eerste_toelating_dt)) ?></th>
                     </tr>
                     <tr>
                         <td colspan="2">Eerste Toelating Nederland:</td>
-                        <th><?php echo date("d-m-Y", strtotime($car->datum_eerste_tenaamstelling_in_nederland)) ?></th>
+                        <th><?php echo date("d-m-Y", strtotime($data['car']->datum_eerste_tenaamstelling_in_nederland)) ?></th>
                     </tr>
                     <tr>
                         <td colspan="2">Laaste Te naamstelling:</td>
-                        <th><?php echo date("d-m-Y", strtotime($car->datum_tenaamstelling_dt)) ?></th>
+                        <th><?php echo date("d-m-Y", strtotime($data['car']->datum_tenaamstelling_dt)) ?></th>
                     </tr>
                     <tr>
                         <td colspan="2">Aantal Wielen:</td>
-                        <th>{{$car->aantal_wielen}}</th>
+                        <th>{{$data['car']->aantal_wielen}}</th>
                     </tr>
                     <tr>
                         <td colspan="2">Aantal Deuren:</td>
-                        <th>{{$car->aantal_deuren}}</th>
+                        <th>{{$data['car']->aantal_deuren}}</th>
                     </tr>
-                    @if(!empty($car->maximum_trekken_massa_ongeremd))
+                    @if(!empty($data['car']->maximum_trekken_massa_ongeremd))
                         <tr>
                             <td colspan="2">Max trekmassa ongeremd:</td>
-                            <th>{{$car->maximum_trekken_massa_ongeremd}} CC</th>
+                            <th>{{$data['car']->maximum_trekken_massa_ongeremd}} CC</th>
                         </tr>
                     @endif
-                    @if(!empty($car->maximum_trekken_massa_ongeremd))
+                    @if(!empty($data['car']->maximum_trekken_massa_ongeremd))
                         <tr>
                             <td colspan="2">Max Trekmassa Geremd:</td>
-                            <th>{{$car->maximum_trekken_massa_geremd}} KG</th>
+                            <th>{{$data['car']->maximum_trekken_massa_geremd}} KG</th>
                         </tr>
                     @endif
 
                     <tr>
                         <td colspan="2">Wam Verzekerd:</td>
-                        <th>{{$car->wam_verzekerd}}</th>
+                        <th>{{$data['car']->wam_verzekerd}}</th>
                     </tr>
                     <tr>
                         <td colspan="2">APK vervaldatum:</td>
-                        <th><?php echo date("d-m-Y", strtotime($car->vervaldatum_apk_dt)) ?></th>
+                        <th><?php echo date("d-m-Y", strtotime($data['car']->vervaldatum_apk_dt)) ?></th>
                     </tr>
-                    @if(!empty($car->cilinderinhoud))
+                    @if(!empty($data['car']->cilinderinhoud))
                         <tr>
                             <td colspan="2">Cilinderinhoud:</td>
-                            <th>{{$car->cilinderinhoud}} CC</th>
+                            <th>{{$data['car']->cilinderinhoud}} CC</th>
                         </tr>
                     @endif
-                    @if(!empty($car->aantal_cilinders))
+                    @if(!empty($data['car']->aantal_cilinders))
                         <tr>
                             <td colspan="2">Aantal cilinders:</td>
-                            <th>{{$car->aantal_cilinders}}</th>
+                            <th>{{$data['car']->aantal_cilinders}}</th>
                         </tr>
                     @endif
                     <tr>
                         <td colspan="2">Kenteken:</td>
-                        <th>{{$car->kenteken}}</th>
+                        <th>{{$data['car']->kenteken}}</th>
                     </tr>
                     <tr>
                         <td colspan="2">Massa Rijklaar:</td>
-                        <th>{{$car->massa_rijklaar}} KG</th>
+                        <th>{{$data['car']->massa_rijklaar}} KG</th>
                     </tr>
                     </tbody>
                 </table>
                 <div class="section-heading"><h4>Apk.</h4></div>
-                @foreach($apks as $apk)
+                @foreach($data['apks'] as $apk)
                     <table class="table table-striped table-hover">
                         <tbody>
                         <tr>
